ed3f7761738d4a4a098d09de4b4bb33815f340d8
yanghoonkim <ad26kt@gmail.com>
2023-12-06 01:59:24 +0900

    Update for nia

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ python tools/lazyconfig_train_net_nia_segmentation.py \
     --config-file projects/ViTDet/configs/NIA/mask_rcnn_vitdet_b_100ep.py \
     --eval-only \
     "dataloader.test.dataset.names='nia_test'" \
-    "train.init_checkpoint='output/segmentation/model_0499999.pth'"
+    "train.init_checkpoint='output/model_final.pth'"
